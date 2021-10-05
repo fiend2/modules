2c06289618b3fcfe6a3165fdb16f72a3efead79d
fiend2 <88592069+fiend2@users.noreply.github.com>
2021-10-05 10:50:24 +0530

    Update main.tf

--- ec2_module/main.tf
+++ ec2_module/main.tf
@@ -1,7 +1,3 @@
-module "sg_module" {
-    source        = "E:/DevOpsSofts/terraform/sg_module"
-}
-
 resource "aws_instance" "testing" {
     ami           = var.ami
     instance_type = var.instance_type
